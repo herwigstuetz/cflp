370591cb2013d9cbbe5b1b32741c4e1a8e5ff84e
Herwig Stuetz <herwig@herwigstuetz.com>
2015-12-27 17:32:43 +0100

    Add plotCFLPFile

--- Plot-Main.hs
+++ Plot-Main.hs
@@ -1,10 +1,26 @@
 module Main where
 
 import Control.Monad (void)
-import System.FilePath ((-<.>))
+import System.FilePath ((-<.>), (</>))
 import System.Environment (getArgs)
 
+
+import           Text.Parsec.Prim
+
+import CFLP
 import CFLPPlot
+
+readCFLPFile :: FilePath -> IO (Maybe CFLP)
+readCFLPFile fileName = do
+  cflp <- readFile fileName
+  return $ case parse cflpFile "cflp" cflp of
+    Left _ -> Nothing
+    Right cflp -> Just cflp
+
+plotCFLPFile :: FilePath -> IO ()
+plotCFLPFile fileName = do
+  Just cflp <- readCFLPFile fileName
+  plotCFLP cflp (fileName -<.> "png")
 
 main :: IO ()
 main = do
@@ -12,4 +28,9 @@ main = do
   case args of
     ("bench" : fileName : [])
       -> void $ readBench fileName (fileName -<.> "png")
+    ("gen" : dirName : [])
+      -> do
+        plotCFLPFile (dirName </> "problem.cflp")
+        plotCFLPFile (dirName </> "exact.cflp")
+        plotCFLPFile (dirName </> "approx.cflp")
     _ -> putStrLn "plot main usage"
